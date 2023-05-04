ce985b77a7a8a7e49aedc024e552501f037f3904
Nicole Orbe <nicoleorbe@outlook.com>
2023-05-04 12:30:09 -0500

    add fullpage.js

---
+++ index.js
@@ -0,0 +1,12 @@
+new fullpage("#fullpage", {
+  // Scrolling
+  autoScrolling: true,
+  scrollHorizontally: true,
+
+  // Navigation
+  anchors: ["firstPage", "secondPage", "thirdPage", "footer"],
+
+  // Design
+  //sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#000"],
+  fitToSection: true,
+});
